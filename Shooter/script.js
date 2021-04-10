ce6a8a9efdb531c1f59db86c4ac9432174d631a0
Yusif Osmanov <o.yusif@hotmail.com>
2021-04-11 01:37:55 +0400

    Shooter is complete

---
+++ Shooter/script.js
@@ -0,0 +1,42 @@
+const gameBoard = () => {
+    let targetShoot = 0;
+    let targetCount = 0;
+    for (let i = 0; i < 25; i++) {
+        let area = document.createElement('div');
+        area.classList.add('area')
+        document.querySelector('.container').appendChild(area)
+    }
+    setInterval(() => {
+        addTarget()
+        targetCount++;
+        document.querySelector('h2').innerText = `Cixan hedeflerin sayi : ${targetCount}`;
+    }, 1000);
+    checkShoot(targetShoot)
+}
+
+const rand = () => {
+    return Math.floor(Math.random() * 26);
+}
+
+const addTarget = () => {
+    let random = rand()
+    let target = document.querySelectorAll('.area')
+    let img = document.createElement('img');
+    img.setAttribute('src', '5-2-target-download-png.png')
+    target[random].appendChild(img);
+    img.setAttribute('data-target', 'target');
+    setInterval(() => {
+        img.style.display = 'none';
+    }, 1000);
+}
+
+const checkShoot = (count) => {
+    document.querySelector('.container').addEventListener('click', (e) => {
+        if (e.target.getAttribute('data-target') == 'target') {
+            e.target.setAttribute('src', '2-2-target-transparent.png');
+            count++
+            document.querySelector('h1').innerText = `Degig vurulan hedef sayi : ${count}`;
+        }
+    })
+}
+gameBoard()
